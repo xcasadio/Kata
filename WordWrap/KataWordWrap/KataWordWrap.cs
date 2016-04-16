e0b1714d31577be24cb8a033eca90c5de4739002
xcasadio <casaprod@hotmail.fr>
2016-04-16 17:17:17 +0200

    kata wordwrap

---
+++ WordWrap/KataWordWrap/KataWordWrap.cs
@@ -0,0 +1,42 @@
+namespace KataWordWrap
+{
+    static class KataWordWrap
+    {
+        public static string Wrap(string text, int columnNumber)
+        {
+            if (IsTextLessThanColumNumber(text, columnNumber))
+            {
+                return text;
+            }
+            else if (IsSpace(text, columnNumber))
+            {
+                return text.Substring(0, columnNumber) + '\n' + Wrap(text.Substring(columnNumber + 1), columnNumber);
+            }
+            else
+            {
+                if (ContainsSpaceBeforeColumnNumber(text, columnNumber))
+                {
+                    return text.Substring(0, text.LastIndexOfAny(new[] { ' ' }, columnNumber)) + '\n' + 
+                           Wrap(text.Substring(text.LastIndexOfAny(new[] { ' ' }, columnNumber) + 1), columnNumber);
+                }
+
+                return text.Substring(0, columnNumber) + '\n' + Wrap(text.Substring(columnNumber), columnNumber);
+            }
+        }
+
+        private static bool IsTextLessThanColumNumber(string text, int columnNumber)
+        {
+            return columnNumber >= text.Length;
+        }
+
+        private static bool IsSpace(string text, int columnNumber)
+        {
+            return text[columnNumber] == ' ';
+        }
+
+        private static bool ContainsSpaceBeforeColumnNumber(string text, int columnNumber)
+        {
+            return text.LastIndexOfAny(new[] {' '}, columnNumber) != -1;
+        }
+    }
+}
---
+++ WordWrap/KataWordWrap/KataWordWrapTests.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NFluent;
+using NUnit.Framework;
+
+namespace KataWordWrap
+{
+    public class KataWordWrapTests
+    {
+        [Test]
+        public void Should_return_word_when_column_number_is_greater_than_the_word_length()
+        {
+            Check.That(KataWordWrap.Wrap("test", 10)).IsEqualTo("test");
+        }
+
+        [Test]
+        public void Should_return_2_lines_when_the_column_number_is_in_a_space()
+        {
+            Check.That(KataWordWrap.Wrap("aaaaa bbbb", 5)).IsEqualTo("aaaaa\nbbbb");
+        }
+
+        [Test]
+        public void Should_return_2_lines_when_the_column_number_is_on_word()
+        {
+            Check.That(KataWordWrap.Wrap("aaaaa bbb", 8)).IsEqualTo("aaaaa\nbbb");
+        }
+
+        [Test]
+        public void Should_return_word_splitted_in_2_lines_when_the_column_number_is_less_than_the_word_length()
+        {
+            Check.That(KataWordWrap.Wrap("aaaaa", 3)).IsEqualTo("aaa\naa");
+        }
+
+        [Test]
+        [TestCase("", 0, "")]
+        [TestCase("", 10, "")]
+        [TestCase("aaa bbb ccc", 3, "aaa\nbbb\nccc")]
+        [TestCase("aaaa bbb ccc", 3, "aaa\na\nbbb\nccc")]
+        public void Should_return_expected(string input, int columnNumber, string expected)
+        {
+            Check.That(KataWordWrap.Wrap(input, columnNumber)).IsEqualTo(expected);
+        }
+    }
+}
